4ce0aef68b2d25b51adea8e409622b52751865ea
Li Shaohua <lishaohua2015@yeah.net>
2015-02-10 08:35:30 +0800

    注册页面修改

--- ibtt/static/js/register.js
+++ ibtt/static/js/register.js
@@ -1,6 +1,17 @@
 /**
  * Created by 001350 on 2015/2/9.
  */
-(function () {
+$(function () {
+    var regster_forms = $('[name="register_form"]');
 
-})
+    $('[name="next_btn"]').on('click', function() {
+        var next_btn = $(this);
+        var index = this.tabIndex;
+        var current_form = $(regster_forms[index]);
+        current_form.fadeIn()
+        var next_form = $(regster_forms[index + 1]);
+        next_form.fadeOut();
+    });
+
+
+});
