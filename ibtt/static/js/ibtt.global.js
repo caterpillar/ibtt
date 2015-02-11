72ca4048ba0ad98dcec2b6a5faa4db92f90730b5
unknown <001350@hw001350.haowu.com>
2015-02-11 17:58:49 +0800

    验证框架的使用方法完善

--- ibtt/static/js/ibtt.global.js
+++ ibtt/static/js/ibtt.global.js
@@ -3,6 +3,8 @@
  */
 
 $(function () {
+
+        $("header").headroom();
     $('.form_date').datetimepicker({
         language: 'zh-CN',
         weekStart: 1,
@@ -13,14 +15,33 @@ $(function () {
         minView: 2,
         forceParse: 0
     });
-
+    //全局的系统校验
     $('form').validate({
             onKeyup: true,
             eachValidField: function () {
                 $(this).closest('div').removeClass('has-error').addClass('has-success');
+                $(this).popover('destroy');
             },
-            eachInvalidField: function () {
+            eachInvalidField: function (event, status, options) {
                 $(this).closest('div').removeClass('has-success').addClass('has-error');
+                $(this).popover('destroy');
+                var errorMsg = $(this).attr('placeholder');
+                if(!status.required) {
+                    errorMsg = '请输入' + errorMsg;
+                } else if(!status.pattern){
+                    errorMsg = errorMsg + '格式不正确';
+                }
+                var options = {
+                    content: errorMsg,
+                    container: 'body',
+                    placement: 'right'
+                }
+                $(this).popover(options);
+                $(this).popover('toggle');
             }
+    });
+
+    $('[role="dialog"]').on('hide.bs.modal', function (e) {
+        $(this).find('input').popover('destroy');
     });
 });
